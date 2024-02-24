4ba9888ccf9544774688d540edc7bb4823d8b718
Rosen Roydev <rosen.roydev@gmail.com>
2024-02-24 13:08:09 +0200

    Add project files.

---
+++ HappyMama/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,94 @@
+﻿
+#nullable disable
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace HappyMama.Areas.Identity.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<LoginModel> _logger;
+
+        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+     
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+     
+
+     
+        public string ReturnUrl { get; set; }
+
+       
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        public class InputModel
+        {
+        
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+          
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            if (!string.IsNullOrEmpty(ErrorMessage))
+            {
+                ModelState.AddModelError(string.Empty, ErrorMessage);
+            }
+
+            returnUrl ??= Url.Content("~/");
+
+            
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+           
+
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            
+
+            if (ModelState.IsValid)
+            {
+                
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, true, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User logged in.");
+                    return LocalRedirect(returnUrl);
+                }
+                
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return Page();
+                }
+            }
+
+            
+            return Page();
+        }
+    }
+}
