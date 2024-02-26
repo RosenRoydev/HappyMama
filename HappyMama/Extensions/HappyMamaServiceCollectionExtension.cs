83a13b2345ff6d01eb343d310894da12ecbfa3b8
Rosen Roydev <rosen.roydev@gmail.com>
2024-02-26 16:29:12 +0200

    ExtensionService added to Program.cs for simplyfying

---
+++ HappyMama/Extensions/HappyMamaServiceCollectionExtension.cs
@@ -0,0 +1,32 @@
+﻿using HappyMama.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class HappyMamaServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
