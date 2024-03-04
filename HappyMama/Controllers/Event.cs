224f0287f871e24c6e105827afb0d68395cc065d
Rosen Roydev <rosen.roydev@gmail.com>
2024-03-04 20:08:38 +0200

    Event Controller is added

---
+++ HappyMama/Controllers/Event.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HappyMama.Controllers
+{
+    public class Event : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
