9cd2907d1d1780c43a0a271ab74166e2d181091d
Rosen Roydev <rosen.roydev@gmail.com>
2024-03-06 16:49:13 +0200

    Add ErrorMesagesConstansts class

---
+++ HappyMama.BusinessLogic/Constants/ErrorMessagesConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace HappyMama.BusinessLogic.Constants
+{
+    public static class ErrorMessagesConstants
+    {
+        public const string RequiredLength = "The {0} field must be between {2} and {1} characters";
+
+        public const string RequiredField = "The field {0} is required";
+
+        public const string NeededAmountRestrict = "The field {0} must be between {1} and {2} BGN";
+    }
+}
